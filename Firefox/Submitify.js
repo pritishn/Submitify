6a83700010e62b12d74c2e4a9a4b87cc63e8fa17
Pritish Priyatosh Nayak <42891296+pritish-n@users.noreply.github.com>
2020-06-05 06:03:36 +0530

    Add files via upload

---
+++ Firefox/Submitify.js
@@ -0,0 +1,64 @@
+let input_el = document.querySelector('input[name="sourceFile"]');
+
+let textarea_el = document.createElement('textarea');
+input_el.getAttributeNames().forEach( attrName => {
+    textarea_el.setAttribute( attrName, 
+    (document.createAttribute( attrName ).value =            
+    input_el.getAttribute( attrName ) )
+    );
+});
+
+input_el.replaceWith(textarea_el);
+
+let cfiles=document.querySelectorAll('.field');
+cfiles[1].innerHTML="Put Code Here:";
+
+let form = document.querySelector('.submitForm');
+let pop_up = document.createAttribute("target");        
+pop_up.value = "_blank";           
+form.setAttributeNode(pop_up);           
+
+// comment-out the below code to remove click to erase on the text-box
+textarea_el.addEventListener('click', function (event) {
+    textarea_el.value = "";
+});
+//---------------------------------------------------------------------
+$(function () {
+    function adjustNotice(programTypeId) {
+        var $programTypeNotice = $(".programTypeNotice");
+        $programTypeNotice.text("");
+                if (programTypeId === 7) {
+                    $programTypeNotice.text("Almost always, if you send a solution on PyPy, it works much faster");
+                }
+                if (programTypeId === 31) {
+                    $programTypeNotice.text("Almost always, if you send a solution on PyPy, it works much faster");
+                }
+    }
+
+            adjustNotice(54);
+
+
+    $("select[name='programTypeId']").change(function () {
+        adjustNotice(parseInt($(this).val()));
+    });
+
+    $(".submit-form, .submitForm").submitOnce(function () {
+        var form = $(this);
+        var $ftaa = form.find("textarea[name='ftaa']");
+        var $bfaa = form.find("textarea[name='bfaa']");
+
+        if (window._ftaa && window._bfaa) {
+            $ftaa.val(window._ftaa);
+            $bfaa.val(window._bfaa);
+        }
+
+        if (form.attr("enctype") === "multipart/form-data") {
+            var sourceFiles = form.find(".table-form textarea[name=sourceFile]");
+
+            if (sourceFiles.length === 1 && sourceFiles[0].files && sourceFiles[0].files.length === 0) {
+                form.removeAttr("enctype");
+            }
+        }
+        return true;
+    });
+});
